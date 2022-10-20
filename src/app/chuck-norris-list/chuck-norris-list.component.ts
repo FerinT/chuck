1250baea397470e91d2fc1ed4c64c6125009546c
Ferin Taylor <f.taylor@humaninference.com>
2022-10-20 16:08:13 +0200

    Polling for jokes

--- src/app/chuck-norris-list/chuck-norris-list.component.ts
+++ src/app/chuck-norris-list/chuck-norris-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ChuckNorrisService} from "../services/chuck-norris/chuck-norris-service";
 import {ChuckNorrisJoke} from "../services/chuck-norris/chuck-norris-joke";
 import {LocalStorageService} from "../services/local-storage/local-storage-service";
@@ -11,19 +11,18 @@ import {LocalStorageService} from "../services/local-storage/local-storage-servi
 export class ChuckNorrisListComponent implements OnInit {
 
   listOfChuckNorrisJokes: ChuckNorrisJoke[] = [];
+  private interval: any;
 
   constructor(private chuckNorrisService: ChuckNorrisService,
-              private localStorageService: LocalStorageService) { }
+              private localStorageService: LocalStorageService) {
+  }
 
   ngOnInit(): void {
     this.populateList();
-    // start timer every 5 seconds
-    // remove the first joke
-    // fetch a new joke
-    // add it to the end
-  }
 
-  clearListOfJokes(): void {
+    this.interval = setInterval(() => {
+      this.getJoke()
+    }, 5 * 1000);
 
   }
 
@@ -41,27 +40,36 @@ export class ChuckNorrisListComponent implements OnInit {
   private getJoke(): void {
     this.chuckNorrisService.getJoke()
       .subscribe(response => {
-         this.listOfChuckNorrisJokes.push(response);
+        if (this.listOfChuckNorrisJokes.length >= 10) {
+          this.listOfChuckNorrisJokes.shift();
+        }
+        this.listOfChuckNorrisJokes.push(response);
       });
   }
 
   addFavourite(newFavourite: ChuckNorrisJoke): void {
     // get list of fav
     // validate if its > 10
-      // if so remove the first and add to the back
+    // if so remove the first and add to the back
     // check for duplicates?
     // can't fetch jokes via endpoint with id?
     let listOfFavourites: ChuckNorrisJoke[] = this.localStorageService.get("list_of_favourites");
 
     // rather do Ternary operation above
-    if(listOfFavourites === null) {
+    if (listOfFavourites === null) {
       listOfFavourites = [];
     }
-    if(listOfFavourites.length >= 10){
+    if (listOfFavourites.length >= 10) {
       listOfFavourites.shift();
     }
     listOfFavourites.push(newFavourite);
     this.localStorageService.set("list_of_favourites", listOfFavourites);
+  }
+
+  ngOnDestroy() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
   }
 
 }
